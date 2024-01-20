10cc00a1ebb6816f940c31722b865f6029912e9b
AliceASouza <alicealvesdev@gmail.com>
2024-01-19 21:53:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let nickNameHeroi = "Guerreira Alada"
+let xpHeroi = 10000
+let nivelHeroi = ""
+
+console.log("Bem vinda " + nickNameHeroi)
+
+if (xpHeroi <= 1000) {
+    nivelHeroi = "Ferro"
+} else if ((xpHeroi >= 1001) && (xpHeroi <= 2000) ){
+    nivelHeroi = "Bronze"
+} else if ((xpHeroi >= 2001) && (xpHeroi <= 5000) ){
+    nivelHeroi = "Prata"
+} else if ((xpHeroi >= 5001) && (xpHeroi <= 7000) ){
+    nivelHeroi = "Ouro"
+} else if ((xpHeroi >= 7001) && (xpHeroi <= 8000) ){
+    nivelHeroi = "Platina"
+} else if ((xpHeroi >= 8001) && (xpHeroi <= 9000) ){
+    nivelHeroi = "Ascendente"
+} else if ((xpHeroi >= 9001) && (xpHeroi <= 10000) ){
+    nivelHeroi = "Imortal"
+} else {
+    nivelHeroi = "Radiante"
+}
+
+console.log("O seu herói com nome " + nickNameHeroi + " está no nível de " + nivelHeroi)
